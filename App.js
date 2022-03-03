fcb6496585c6e6b8ba8bfcf3631f5688a9ca088b
ngodingbentar <dewaqintoro@gmail.com>
2022-03-03 21:05:38 +0700

    search comp

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ import {
   useColorScheme,
   Image,
   View,
+  TextInput,
 } from 'react-native';
 
 import {
@@ -31,7 +32,17 @@ export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'blue'}}></View>
+        <View style={{flex: 1, backgroundColor: 'white'}}>
+          <View style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+            <View style={{position: 'relative', flex: 1}}>
+              <TextInput placeholder='What do ypu want to eat?' style={{ borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18 }} />
+              <Image source={require('./icon/search.png')} style={{position: 'absolute', top: 5, left:12 }} />
+            </View>
+            <View style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+              <Image source={require('./icon/promo.png')} />
+            </View>
+          </View>
+        </View>
         <View style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
           <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')} />

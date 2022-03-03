ca757e64d43bef2fea05ee2f017bc8ecbe9a8733
ngodingbentar <dewaqintoro@gmail.com>
2022-03-03 22:52:37 +0700

    gopay section

--- App.js
+++ App.js
@@ -1,13 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React, {Component} from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -20,49 +11,123 @@ import {
   TextInput,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
 export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
         <View style={{flex: 1, backgroundColor: 'white'}}>
-          <View style={{marginHorizontal: 17, flexDirection: 'row', paddingTop: 15}}>
+          <View
+            style={{
+              marginHorizontal: 17,
+              flexDirection: 'row',
+              paddingTop: 15,
+            }}>
             <View style={{position: 'relative', flex: 1}}>
-              <TextInput placeholder='What do ypu want to eat?' style={{ borderWidth: 1, borderColor: '#E8E8E8', borderRadius: 25, height: 40, fontSize: 13, paddingLeft: 45, paddingRight: 20, backgroundColor: 'white', marginRight: 18 }} />
-              <Image source={require('./icon/search.png')} style={{position: 'absolute', top: 5, left:12 }} />
+              <TextInput
+                placeholder="What do ypu want to eat?"
+                style={{
+                  borderWidth: 1,
+                  borderColor: '#E8E8E8',
+                  borderRadius: 25,
+                  height: 40,
+                  fontSize: 13,
+                  paddingLeft: 45,
+                  paddingRight: 20,
+                  backgroundColor: 'white',
+                  marginRight: 18,
+                }}
+              />
+              <Image
+                source={require('./icon/search.png')}
+                style={{position: 'absolute', top: 5, left: 12}}
+              />
             </View>
-            <View style={{width: 35, alignItems: 'center', justifyContent: 'center'}}>
+            <View
+              style={{
+                width: 35,
+                alignItems: 'center',
+                justifyContent: 'center',
+              }}>
               <Image source={require('./icon/promo.png')} />
             </View>
           </View>
+          {/* Gopay */}
+          <View style={{marginHorizontal: 17, marginTop: 8}}>
+            <View style={{flexDirection: 'row', justifyContent: 'space-between', backgroundColor: '#2C5FB8', borderTopLeftRadius: 4, borderTopRightRadius: 4, padding: 14}}>
+              <Image source={require('./icon/gopay.png')} />
+              <Text style={{fontSize: 17, fontWeight: 'bold', color: 'white'}}>Rp. 94.122</Text>
+            </View>
+            <View style={{flexDirection: 'row', paddingTop: 20, paddingBottom: 14, backgroundColor: '#2F65BD', borderBottomLeftRadius: 4, borderBottomRightRadius: 4}}>
+              <View style={{flex: 1, alignItems: 'center'}}>
+                <Image source={require('./icon/pay.png')} />
+                <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', marginTop: 15}}>Pay</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center'}}>
+                <Image source={require('./icon/nearby.png')} />
+                <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', marginTop: 15}}>nearby</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center'}}>
+                <Image source={require('./icon/topup.png')} />
+                <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', marginTop: 15}}>topup</Text>
+              </View>
+              <View style={{flex: 1, alignItems: 'center'}}>
+                <Image source={require('./icon/more.png')} />
+                <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', marginTop: 15}}>more</Text>
+              </View>
+            </View>
+          </View>
         </View>
-        <View style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Image style={{width: 26, height: 26}} source={require('./icon/home-active.png')} />
-            <Text style={{fontSize: 10, color: '#43AB4A', marginTop: 4,}}>Home</Text>
+        <View
+          style={{height: 54, flexDirection: 'row', backgroundColor: 'white'}}>
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./icon/home-active.png')}
+            />
+            <Text style={{fontSize: 10, color: '#43AB4A', marginTop: 4}}>
+              Home
+            </Text>
           </View>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Image style={{width: 26, height: 26}} source={require('./icon/order.png')} />
-            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>Orders</Text>
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./icon/order.png')}
+            />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+              Orders
+            </Text>
           </View>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Image style={{width: 26, height: 26}} source={require('./icon/help.png')} />
-            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>help</Text>
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./icon/help.png')}
+            />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+              help
+            </Text>
           </View>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Image style={{width: 26, height: 26}} source={require('./icon/inbox.png')} />
-            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>inbox</Text>
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./icon/inbox.png')}
+            />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+              inbox
+            </Text>
           </View>
-          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Image style={{width: 26, height: 26}} source={require('./icon/account.png')} />
-            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>account</Text>
+          <View
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Image
+              style={{width: 26, height: 26}}
+              source={require('./icon/account.png')}
+            />
+            <Text style={{fontSize: 10, color: '#545454', marginTop: 4}}>
+              account
+            </Text>
           </View>
         </View>
       </View>

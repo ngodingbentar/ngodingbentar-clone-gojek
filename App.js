4a0939b548086be4d4672b8c2c7fcba99b0f1ce9
ngodingbentar <dewaqintoro@gmail.com>
2022-04-10 22:33:12 +0700

    added: component gobanner,goinfo,gonews,search

--- App.js
+++ App.js
@@ -9,49 +9,19 @@ import {
   ScrollView,
   TouchableOpacity,
 } from 'react-native';
+import GoBanner from './src/components/molecules/GoBanner';
+import GoInfo from './src/components/molecules/GoInfo';
+import GoNews from './src/components/molecules/GoNews';
 import GopayFeature from './src/components/molecules/GopayFeature';
 import MainFeature from './src/components/molecules/MainFeature';
+import SearchFeature from './src/components/molecules/SearchFeature';
 export default class App extends Component {
   render() {
     return (
       <View style={{flex: 1}}>
         <View style={{flex: 1, backgroundColor: 'white'}}>
           {/* Searchbar */}
-          <View
-            style={{
-              marginHorizontal: 17,
-              flexDirection: 'row',
-              paddingTop: 15,
-            }}>
-            <View style={{position: 'relative', flex: 1}}>
-              <TextInput
-                placeholder="What do ypu want to eat?"
-                style={{
-                  borderWidth: 1,
-                  borderColor: '#E8E8E8',
-                  borderRadius: 25,
-                  height: 40,
-                  fontSize: 13,
-                  paddingLeft: 45,
-                  paddingRight: 20,
-                  backgroundColor: 'white',
-                  marginRight: 18,
-                }}
-              />
-              <Image
-                source={require('./src/assets/icon/search.png')}
-                style={{position: 'absolute', top: 5, left: 12}}
-              />
-            </View>
-            <View
-              style={{
-                width: 35,
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}>
-              <Image source={require('./src/assets/icon/promo.png')} />
-            </View>
-          </View>
+          <SearchFeature />
           <ScrollView>
             {/* Gopay */}
             <View style={{marginHorizontal: 17, marginTop: 8}}>
@@ -151,253 +121,13 @@ export default class App extends Component {
             />
 
             {/* News section */}
-            <View style={{paddingTop: 16, paddingHorizontal: 16}}>
-              <View style={{position: 'relative'}}>
-                <Image
-                  source={require('./src/assets/dummy/sepak-bola.jpg')}
-                  style={{height: 170, width: '100%', borderRadius: 6}}
-                />
-                <View
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    backgroundColor: 'black',
-                    opacity: 0.3,
-                    borderRadius: 6,
-                  }}
-                />
-                <View
-                  style={{
-                    height: 15,
-                    width: 60,
-                    position: 'absolute',
-                    top: 16,
-                    left: 16,
-                  }}>
-                  <Image
-                    source={require('./src/assets/logo/white.png')}
-                    style={{
-                      height: undefined,
-                      width: undefined,
-                      resizeMode: 'contain',
-                      flex: 1,
-                    }}
-                  />
-                </View>
-              </View>
-              <View
-                style={{
-                  paddingTop: 16,
-                  paddingBottom: 20,
-                  borderBottomColor: '#E8E9ED',
-                  borderBottomWidth: 1,
-                }}>
-                <Text
-                  style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C'}}>
-                  GO-NEWS
-                </Text>
-                <Text
-                  style={{
-                    fontSize: 14,
-                    fontWeight: 'normal',
-                    color: '#7A7A7A',
-                    marginBottom: 11,
-                  }}>
-                  Dimas Drajat selamatkan pinalti, Timnas U-23 kalahkan Brunei
-                </Text>
-                <TouchableOpacity
-                  style={{
-                    backgroundColor: '#61A756',
-                    paddingHorizontal: 12,
-                    paddingVertical: 11,
-                    alignSelf: 'flex-end',
-                    borderRadius: 4,
-                  }}>
-                  <Text
-                    style={{
-                      fontSize: 13,
-                      fontWeight: 'bold',
-                      color: 'white',
-                      textAlign: 'center',
-                    }}>
-                    READ
-                  </Text>
-                </TouchableOpacity>
-              </View>
-            </View>
+            <GoNews img={require('./src/assets/dummy/sepak-bola.jpg')} />
 
             {/* internal information section */}
-            <View style={{padding: 16, paddingBottom: 0}}>
-              <View style={{height: 15, width: 60, marginLeft: -4}}>
-                <Image
-                  source={require('./src/assets/logo/gojek.png')}
-                  style={{
-                    height: undefined,
-                    width: undefined,
-                    resizeMode: 'contain',
-                    flex: 1,
-                  }}
-                />
-              </View>
-              <Text
-                style={{
-                  fontSize: 17,
-                  fontWeight: 'bold',
-                  color: '#1C1C1C',
-                  marginTop: 15,
-                  marginBottom: 20,
-                }}>
-                Complete your profile
-              </Text>
-              <View style={{flexDirection: 'row', marginBottom: 16}}>
-                <View>
-                  <Image
-                    source={require('./src/assets/dummy/facebook-connect.png')}
-                  />
-                </View>
-                <View style={{marginLeft: 16, flex: 1}}>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      fontWeight: 'bold',
-                      color: '#4A4A4A',
-                    }}>
-                    Connect with facebook
-                  </Text>
-                  <Text
-                    style={{
-                      fontSize: 15,
-                      fontWeight: 'normal',
-                      color: '#4A4A4A',
-                      width: '70%',
-                    }}>
-                    Login faster without verification code
-                  </Text>
-                </View>
-              </View>
-              <TouchableOpacity
-                style={{
-                  backgroundColor: '#61A756',
-                  paddingHorizontal: 12,
-                  paddingVertical: 11,
-                  alignSelf: 'flex-end',
-                  borderRadius: 4,
-                }}>
-                <Text
-                  style={{
-                    fontSize: 13,
-                    fontWeight: 'bold',
-                    color: 'white',
-                    textAlign: 'center',
-                  }}>
-                  CONNECT
-                </Text>
-              </TouchableOpacity>
-              <View
-                style={{
-                  borderBottomColor: '#E8E9ED',
-                  borderBottomWidth: 1,
-                  marginTop: 16,
-                }}
-              />
-            </View>
+            <GoInfo />
 
-            {/* gofood banner section */}
-            <View style={{padding: 16}}>
-              <View style={{position: 'relative'}}>
-                <Image
-                  source={require('./src/assets/dummy/food-banner.jpg')}
-                  style={{height: 170, width: '100%', borderRadius: 6}}
-                />
-                <View
-                  style={{
-                    width: '100%',
-                    height: '100%',
-                    position: 'absolute',
-                    top: 0,
-                    left: 0,
-                    backgroundColor: 'black',
-                    opacity: 0.15,
-                    borderRadius: 6,
-                  }}
-                />
-                <View
-                  style={{
-                    height: 15,
-                    width: 60,
-                    position: 'absolute',
-                    top: 16,
-                    left: 16,
-                  }}>
-                  <Image
-                    source={require('./src/assets/logo/white.png')}
-                    style={{
-                      height: undefined,
-                      width: undefined,
-                      resizeMode: 'contain',
-                      flex: 1,
-                    }}
-                  />
-                </View>
-                <View
-                  style={{
-                    position: 'absolute',
-                    bottom: 0,
-                    left: 0,
-                    width: '100%',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    paddingHorizontal: 16,
-                    paddingBottom: 16,
-                  }}>
-                  <View>
-                    <Text
-                      style={{
-                        fontSize: 18,
-                        fontWeight: 'bold',
-                        color: 'white',
-                        marginBottom: 8,
-                      }}>
-                      Free GO-FOODNvoucher
-                    </Text>
-                    <Text
-                      style={{fontSize: 12, fontWeight: '400', color: 'white'}}>
-                      Quick, before they run out!
-                    </Text>
-                  </View>
-                  <View style={{flex: 1, paddingLeft: 12}}>
-                    <TouchableOpacity
-                      style={{
-                        backgroundColor: '#61A756',
-                        paddingHorizontal: 12,
-                        paddingVertical: 11,
-                        alignSelf: 'stretch',
-                        borderRadius: 4,
-                      }}>
-                      <Text
-                        style={{
-                          fontSize: 12,
-                          fontWeight: 'bold',
-                          color: 'white',
-                          textAlign: 'center',
-                        }}>
-                        GET VOUCHER
-                      </Text>
-                    </TouchableOpacity>
-                  </View>
-                </View>
-              </View>
-              <View
-                style={{
-                  borderBottomColor: '#E8E9ED',
-                  borderBottomWidth: 1,
-                  marginTop: 16,
-                }}
-              />
-            </View>
+            {/* go-banner section */}
+            <GoBanner img={require('./src/assets/dummy/food-banner.jpg')} />
             {/* end gofood banner */}
 
             {/* Nearby gofood */}
---
+++ src/components/molecules/GoNews/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {Text, Image, View, TouchableOpacity} from 'react-native';
+
+const GoNews = props => {
+  return (
+    <View style={{paddingTop: 16, paddingHorizontal: 16}}>
+      <View style={{position: 'relative'}}>
+        <Image
+          source={props.img}
+          style={{height: 170, width: '100%', borderRadius: 6}}
+        />
+        <View
+          style={{
+            width: '100%',
+            height: '100%',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            backgroundColor: 'black',
+            opacity: 0.3,
+            borderRadius: 6,
+          }}
+        />
+        <View
+          style={{
+            height: 15,
+            width: 60,
+            position: 'absolute',
+            top: 16,
+            left: 16,
+          }}>
+          <Image
+            source={require('../../../assets/logo/white.png')}
+            style={{
+              height: undefined,
+              width: undefined,
+              resizeMode: 'contain',
+              flex: 1,
+            }}
+          />
+        </View>
+      </View>
+      <View
+        style={{
+          paddingTop: 16,
+          paddingBottom: 20,
+          borderBottomColor: '#E8E9ED',
+          borderBottomWidth: 1,
+        }}>
+        <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C'}}>
+          GO-NEWS
+        </Text>
+        <Text
+          style={{
+            fontSize: 14,
+            fontWeight: 'normal',
+            color: '#7A7A7A',
+            marginBottom: 11,
+          }}>
+          Dimas Drajat selamatkan pinalti, Timnas U-23 kalahkan Brunei
+        </Text>
+        <TouchableOpacity
+          style={{
+            backgroundColor: '#61A756',
+            paddingHorizontal: 12,
+            paddingVertical: 11,
+            alignSelf: 'flex-end',
+            borderRadius: 4,
+          }}>
+          <Text
+            style={{
+              fontSize: 13,
+              fontWeight: 'bold',
+              color: 'white',
+              textAlign: 'center',
+            }}>
+            READ
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default GoNews;
